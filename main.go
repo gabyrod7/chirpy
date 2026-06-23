456ef871d537a13f2d21d246106032abbf1e96bc
Gabriel Rodriguez <gabyrod7@gmail.com>
2026-06-23 21:56:18 +0200

    Checkpoint CH2 Completed

--- main.go
+++ main.go
@@ -1,17 +1,28 @@
 package main
 
 import (
+    "fmt"
 	"log"
 	"net/http"
+    "sync/atomic"
 )
+
+type apiConfig struct {
+    fileserverHits atomic.Int32
+}
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
-	mux.HandleFunc("/healthz", handlerReadiness)
+
+    apiCfg := apiConfig{fileserverHits: atomic.Int32{},}
+    handler := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
+    mux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
+	mux.HandleFunc("GET /healthz", handlerReadiness)
+	mux.HandleFunc("GET /metrics", apiCfg.handlerCount)
+	mux.HandleFunc("POST /reset", apiCfg.handlerCountReset)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -28,3 +39,22 @@ func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
 
+func (cfg *apiConfig) handlerCount(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+    //w.Write([]byte("Hits: "+cfg.fileserverHits))
+    w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) handlerCountReset(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+    cfg.fileserverHits.Store(0)
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        cfg.fileserverHits.Add(1)
+        next.ServeHTTP(w, r)
+    })
+}
