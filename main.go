d4d10f8ed6b49aca1f5fdb5d6b1567beca193df1
Gabriel Rodriguez <gabyrod7@gmail.com>
2026-06-23 15:15:14 +0200

    Checkpoint CH1 L9 complete

--- main.go
+++ main.go
@@ -1,13 +1,30 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
 func main() {
-	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	const filepathRoot = "."
+	const port = "8080"
 
-	server := &http.Server{Handler : mux, Addr : ":8080"}
-	server.ListenAndServe()
+	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", handlerReadiness)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
 }
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
