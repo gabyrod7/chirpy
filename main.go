cf614afa2bfb5d94919d7cffcb0e136796820b8a
Gabriel Rodriguez <gabyrod7@gmail.com>
2026-06-25 22:36:55 +0200

    Checkpoint CH4 L3 completed

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
     "fmt"
 	"log"
+	"encoding/json"
 	"net/http"
     "sync/atomic"
 )
@@ -20,9 +21,10 @@ func main() {
     apiCfg := apiConfig{fileserverHits: atomic.Int32{},}
     handler := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
     mux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
-	mux.HandleFunc("GET /healthz", handlerReadiness)
-	mux.HandleFunc("GET /metrics", apiCfg.handlerCount)
-	mux.HandleFunc("POST /reset", apiCfg.handlerCountReset)
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("POST /api/validate_chirp", handlerValidChirp)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerCount)
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerCountReset)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -40,10 +42,17 @@ func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 }
 
 func (cfg *apiConfig) handlerCount(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Add("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-    //w.Write([]byte("Hits: "+cfg.fileserverHits))
-    w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+    //w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+	text := fmt.Sprintf(`<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>
+`, cfg.fileserverHits.Load())
+	w.Write([]byte(text))
 }
 
 func (cfg *apiConfig) handlerCountReset(w http.ResponseWriter, r *http.Request) {
@@ -57,4 +66,54 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
         cfg.fileserverHits.Add(1)
         next.ServeHTTP(w, r)
     })
+}
+
+func handlerValidChirp(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:body"`
+	}
+	type errorResponse struct {
+		Error string `json:"error"`
+	}
+	type validResponse struct {
+		Valid bool `json:"valid"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		log.Printf("Error decoding parameters: %s", err)
+		//w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if len(params.Body) > 140 {
+		resp := errorResponse{
+			Error: "Chirp is too long",
+		}
+		dat, err := json.Marshal(resp)
+		if err != nil {
+			w.WriteHeader(500)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(dat)
+		return
+	}
+
+	resp := validResponse{
+		Valid: true,
+	}
+	dat, err := json.Marshal(resp)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(500)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(dat)
 }
