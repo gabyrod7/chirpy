1cdc9ea95f1c6c0aefdcd91fd1b72fa65a976ab8
Gabriel Rodriguez <gabyrod7@gmail.com>
2026-06-28 21:17:22 +0200

    Checkpoit CH5 L3 completed

--- main.go
+++ main.go
@@ -1,25 +1,50 @@
 package main
 
 import (
+	"os"
     "fmt"
 	"log"
 	"strings"
+	"database/sql"
 	"encoding/json"
 	"net/http"
     "sync/atomic"
+
+	"github.com/joho/godotenv"
+
+	"github.com/gabyrod7/chirpy/internal/database"
 )
+
+import _ "github.com/lib/pq"
 
 type apiConfig struct {
     fileserverHits atomic.Int32
+	db *database.Queries
 }
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL must be set")
+	}
+
+	dbConn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Error opening database: %s", err)
+	}
+	dbQueries := database.New(dbConn)
+
+    apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+		db: dbQueries,
+	}
+
 	mux := http.NewServeMux()
 
-    apiCfg := apiConfig{fileserverHits: atomic.Int32{},}
     handler := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
     mux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
