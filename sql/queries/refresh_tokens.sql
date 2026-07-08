22d59d3433eb7287dbd9993fe9de78e930eb18ff
Gabriel Rodriguez <gabyrod7@gmail.com>
2026-07-08 13:22:26 +0400

    styling

--- sql/queries/refresh_tokens.sql
+++ sql/queries/refresh_tokens.sql
@@ -10,8 +10,8 @@ VALUES (
 RETURNING *;
 
 -- name: RevokeRefreshToken :one
-UPDATE refresh_tokens SET revoked_at = NOW(),
-updated_at = NOW()
+UPDATE refresh_tokens 
+SET revoked_at = NOW(), updated_at = NOW()
 WHERE token = $1
 RETURNING *;
 
